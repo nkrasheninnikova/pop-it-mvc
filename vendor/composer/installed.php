d61a166b8039acac475d441f1550055f9997b9cb
Natasha Krasheninnikova <nk110905@mail.ru>
2025-04-07 23:45:17 +0700

    composer commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'nkrasheninnikova/pop-it-mvc',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '7837bfed4e4635d04406e0342f0528a0809d9b31',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'nkrasheninnikova/pop-it-mvc' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '7837bfed4e4635d04406e0342f0528a0809d9b31',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
